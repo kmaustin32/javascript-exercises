12756ddfc9b712be467f1da1ec5ca22dd8a90108
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-01-02 19:23:29 +0200

    Complete JS exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let punctuation = /[.,?!\s]/g;
+  let newString = string.replace(punctuation, '').toLowerCase();
+  
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] != newString[newString.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
