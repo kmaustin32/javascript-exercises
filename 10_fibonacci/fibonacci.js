1cfb3b35312ba2a010c8c7cdc0096bd0f4f86399
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-01-03 11:04:41 +0200

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let numInt = parseInt(num);
 
+  if (numInt == 0) return 0;
+  if (numInt == 1 || numInt == 2) return 1;
+  if (numInt < 0) return "OOPS";
+
+  let prevNum = 1;
+  let prevNum2 = 1
+  let fibNum = null;
+  for ( let i = 2; i < num; i++) {
+    fibNum = prevNum + prevNum2;
+    prevNum = prevNum2;
+    prevNum2 = fibNum;
+  }
+  return fibNum;
 };
 
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
